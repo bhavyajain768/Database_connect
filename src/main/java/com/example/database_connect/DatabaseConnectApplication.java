72dfa724f454982c4c1591d992af6858e5a31047
Bhavya <bhavyajain768@gmail.com>
2023-03-02 17:41:25 +0530

    Creating Table

---
+++ src/main/java/com/example/database_connect/DatabaseConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.database_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseConnectApplication.class, args);
+    }
+
+}
